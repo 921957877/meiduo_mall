3fb4d6c427488a23248d38a77bb426b950f3c6cf
xwp <xwp_fullstack@163.com>
2019-04-18 19:03:02 +0800

    第一天

---
+++ meiduo_mall/meiduo_mall/apps/users/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+# Create your models here.
+# 我们重写用户模型类,继承自AbstractUser
+class User(AbstractUser):
+    """自定义用户模型类"""
+    # 在用户模型类中增加mobilephone字段
+    mobilephone = models.CharField(max_length=11, verbose_name='手机号', unique=True)
+
+    # 对当前表进行相关设置
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name = '用户'
+        verbose_name_plural = verbose_name
+
+    # 在str魔法方法中返回用户名称
+    def __str__(self):
+        return self.username
---
+++ meiduo_mall/meiduo_mall/apps/users/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from meiduo_mall.apps.users import views
+
+urlpatterns = [
+    url(r'^register/$', views.RegisterView.as_view(), name='register')  # 注册
+]
