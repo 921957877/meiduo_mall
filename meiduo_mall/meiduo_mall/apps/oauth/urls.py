ce0eaf8e7adbdc86ea4bf32030dbc545ed6e3f2c
XinYiFei <bhmb9@qq.com>
2019-04-24 16:22:14 +0800

    QQ登陆第一个接口

--- meiduo_mall/meiduo_mall/apps/oauth/urls.py
+++ meiduo_mall/meiduo_mall/apps/oauth/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-
+    # 获取QQ登陆页面网址
+    url(r'^qq/authorization/$', views.QQUrlView.as_view()),
 ]
--- meiduo_mall/meiduo_mall/apps/oauth/views.py
+++ meiduo_mall/meiduo_mall/apps/oauth/views.py
@@ -1,3 +1,24 @@
+from QQLoginTool.QQtool import OAuthQQ
+from django import http
+from django.conf import settings
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
+
+from meiduo_mall.utils.response_code import RETCODE
+
+
+class QQUrlView(View):
+    """提供QQ登陆页面网址"""
+
+    def get(self, request):
+        # 1.接收参数(该参数不是必传)
+        next = request.GET.get('next')
+        # 2.创建工具类(QQLoginTool)的对象
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID, client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI, state=next)
+        # 3.调用对象生成QQ登陆网址的方法
+        login_url = oauth.get_qq_url()
+        # 4.返回登陆地址
+        return http.JsonResponse({'code': RETCODE.OK, 'errmsg': 'ok', 'login_url': login_url})
